d7ad426b0ddcf230123f4e84e73afb09bbf66ee9
jTommy786 <jose.julian.bonilla786@gmail.com>
2026-01-07 21:47:38 -0500

    #4 JB

--- SistemaCursosMatriculas_MPJGJB/src/udla/mpjgjb/util/ConexionDB.java
+++ SistemaCursosMatriculas_MPJGJB/src/udla/mpjgjb/util/ConexionDB.java
@@ -7,23 +7,16 @@ import java.sql.SQLException;
 /**
  * Clase ConexionDB
  * Gestiona la conexión a la base de datos MySQL
- * Conceptos POO: Encapsulamiento
- * @author Julián
  */
 public class ConexionDB {
 
     // Configuración de la conexión a MySQL
     private static final String URL = "jdbc:mysql://localhost:3306/cursos_db?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "root";        // Cambiar según la configuración local
+    private static final String USER = "root";        // Cambiar según la configuración de sus compus
     private static final String PASSWORD = "Julianbonilla1";        // Cambiar según la contraseña local
 
-    /**
-     * Obtiene una conexión a la base de datos
-     * @return Conexión a MySQL
-     * @throws SQLException Si hay error en la conexión
-     */
+    
     public static Connection getConexion() throws SQLException {
-        // JDBC 4+ registra automáticamente el driver si está en el classpath
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
